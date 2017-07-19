d5fc6be220e4d6c98ef1649c58a3b9dd5f43de83
river <huangdaj@dongpeng.net>
2017-07-19 16:48:43 +0800

    错误码定义

--- dpmall-api/src/main/java/com/dpmall/api/err/ErrorCode.java
+++ dpmall-api/src/main/java/com/dpmall/api/err/ErrorCode.java
@@ -8,5 +8,6 @@ package com.dpmall.api.err;
  */
 public class ErrorCode {
     public static final int SUCCESS = 200;
-    public static final int ERROR = 500;
+    public static final int INTERNAL_ERR = 500;
+    public static final int INVALID_PARAM = 510;
 }
