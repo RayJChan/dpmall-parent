ad94757b9373991065f31d13bc04bcae5fefe11b
river <huangdaj@dongpeng.net>
2017-07-20 17:55:10 +0800

    增加装修风格

--- dpmall-api/src/main/java/com/dpmall/api/enums/EDecStyle.java
+++ dpmall-api/src/main/java/com/dpmall/api/enums/EDecStyle.java
@@ -1,5 +1,11 @@
 package com.dpmall.api.enums;
 
+/**
+ * <p>
+ * 装修风格
+ * @author river
+ * @date 2017-07-20
+ */
 public enum EDecStyle {
 	RETRO_CH("retro","复古中式"),
 	FRENCH_STYLE("french","法式风格"),
@@ -12,18 +18,17 @@ public enum EDecStyle {
     ELEGANT_STYLE("elegant","精致风格"),
     NATURE_STYLE("nature","自然风格"),
     BRISK_STYLE("brisk","轻快风格"),
-    SOFT_STYLE("","");
-//	柔和风格
-//	都市风格
-//	清新风格
-//	现代风格
-//	田园风格
-//	后现代风格
-//	中式风格
-//	欧式古典风格
-//	地中海风格
-//	东南亚风格
-//	日式风格
+    SOFT_STYLE("soft","柔和风格"),
+	URBAN_STYLE("urban","都市风格"),
+	FRESH_STYLE("fresh","清新风格"),
+	MODERN_STYLE("modern","现代风格"),
+	VILLAGE_STYLE("village","田园风格"), 
+	POSTMODERN_STYLE("postmodern","后现代风格"),
+	CHINESE_STYLE("chinese","中式风格"),
+	EUR_CLASSIC_STYLE("eur_classic","欧式古典"),
+	MEDITERRANEAN("mediterranean","地中海风格"),
+	SOUTH_EAST_ASIAN("south_east_asian","东南亚风格"),
+	JAPANESE_STYLE("japanese_style","日式风格");
 	
     private String code;
 	
--- dpmall-api/src/main/java/com/dpmall/api/enums/ERoleType.java
+++ dpmall-api/src/main/java/com/dpmall/api/enums/ERoleType.java
@@ -1,9 +1,16 @@
 package com.dpmall.api.enums;
 
+/**
+ * 角色类型
+ * @author river
+ * @date 2017-07-20
+ */
 public enum ERoleType {
-	
+	/**店员*/
 	STORE_USER(1,"store_user"),
+	/**店长*/
 	STORE_MGR(2,"store_mgr"),
+	/**经销商*/
 	AGENCY(3,"agency");
 	
     private Integer value;
