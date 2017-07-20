2d1f9104f838fdb16117f19b1e80327d95bd4ef2
river <huangdaj@dongpeng.net>
2017-07-20 19:47:28 +0800

    Serializable

--- dpmall-api/src/main/java/com/dpmall/api/param/SaleLeadStatisticForm.java
+++ dpmall-api/src/main/java/com/dpmall/api/param/SaleLeadStatisticForm.java
@@ -1,11 +1,15 @@
 package com.dpmall.api.param;
 
+import java.io.Serializable;
+
 /**
  * 统计参数
  * @author river
  * @date 2017-07-18
  */
-public class SaleLeadStatisticForm {
+public class SaleLeadStatisticForm implements Serializable{
+	private static final long serialVersionUID = 2718302718019889798L;
+
 	/**门店ID*/
     public Long storeId;
     
