f72ce07bc5dcb3c5cc5dc11bf3aebad07f7f7189
sklong1234 <1013760547@qq.com>
2017-08-02 10:06:30 +0800

    xiecong

--- dpmall-db/src/main/java/com/dpmall/db/dao/PrePayDao.java
+++ dpmall-db/src/main/java/com/dpmall/db/dao/PrePayDao.java
@@ -1,5 +1,7 @@
 package com.dpmall.db.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface PrePayDao {
-	
+	Integer get2DistributeCount(@Param("distributorId")String distributorId,@Param("status")String status);
 }
