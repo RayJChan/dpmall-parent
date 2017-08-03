eb3a45eb550d001ff4efb6887f3a56bb9a5e8fe8
daihongxu <daihx@dongpeng.net>
2017-08-03 13:54:18 +0800

    JSON添加

--- dpmall-web/src/main/java/com/dpmall/web/controller/PrepayController.java
+++ dpmall-web/src/main/java/com/dpmall/web/controller/PrepayController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.dpmall.api.IPrepayService;
 import com.dpmall.api.err.ErrorCode;
 import com.dpmall.web.controller.form.AppPrepayForm;
